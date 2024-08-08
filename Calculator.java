b0b0f0473eb06672ba6716d0f7e74f2c4eac63f7
Prashant K Pathak <123973637+prashant152005@users.noreply.github.com>
2024-08-08 23:57:00 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,12 @@ import java.lang.Math;
 
 public class Calculator {
 
-    
+    private static final int WINDOW_WIDTH = 410;
+    private static final int WINDOW_HEIGHT = 600;
+    private static final int BUTTON_WIDTH = 80;
+    private static final int BUTTON_HEIGHT = 70;
+    private static final int MARGIN_X = 20;
+    private static final int MARGIN_Y = 60;
 
     private JFrame window; // Main window
     private JComboBox<String> comboCalcType, comboTheme;
